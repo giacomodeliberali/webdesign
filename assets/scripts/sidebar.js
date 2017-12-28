f67135983fbb091b6d4c96d7d2893b1e7d5196e5
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2017-12-28 19:45:32 +0100

    Fixed articles spacing

--- assets/scripts/sidebar.js
+++ assets/scripts/sidebar.js
@@ -24,9 +24,17 @@ $(document).ready(function () {
     });
 
     // perfect scrollbar
-    var navbar = new PerfectScrollbar('nav.navbar');
-    var articlesContainer = new PerfectScrollbar('#articles-container');
-    var rightArticlesContainer = new PerfectScrollbar('#right-articles-container');
+    // var navbar = new PerfectScrollbar('nav.navbar');
+    // var articlesContainer = new PerfectScrollbar('#articles-container');
+    // var rightArticlesContainer = new PerfectScrollbar('#right-articles-container');
+    // var mainContainer = new PerfectScrollbar('.container.main');
+
+    // window.onresize = function(){
+    //     navbar.update();
+    //     articlesContainer.update();
+    //     rightArticlesContainer.update();
+    //     mainContainer.update();
+    // };
 
     /* Make the input width fixed until click outside */
     $("#search").on("click",function(){
@@ -35,5 +43,24 @@ $(document).ready(function () {
     $("#search").on("blur",function(){
         $(this).closest(".form-group").removeClass("hover");
     });
+
+    scrollFunction();
+    // When the user scrolls down 20px from the top of the document, show the button
+    $(".container.main").scroll(scrollFunction);
+
+    $("#backToTop").on("click",topFunction);
 });
 
+
+function scrollFunction() {
+    if ($(".container.main").scrollTop() > 20) {
+        document.getElementById("backToTop").style.display = "block";
+    } else {
+        document.getElementById("backToTop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    $(".container.main").animate({scrollTop: 0}, 200);
+}
