fdd0f95cfd8dfbb0db4c2079e95dd3120767588f
luckyunlock <857392@stud.unive.it>
2018-01-14 14:04:13 +0100

    Inseriti script nella pagina di ricerca

---
+++ assets/scripts/retrive_arguments.js
@@ -0,0 +1,32 @@
+function parseGetVars()
+{
+  alert("1");
+    // creo una array
+  var args = new Array();
+  // individuo la query (cioè tutto quello che sta a destra del ?)
+  // per farlo uso il metodo substring della proprietà search
+  // dell'oggetto location
+  var query = window.location.search.substring(1);
+  // se c'è una querystring procedo alla sua analisi
+  if (query)
+  {
+    // divido la querystring in blocchi sulla base del carattere &
+    // (il carattere & è usato per concatenare i diversi parametri della URL)
+    var strList = query.split('&');
+    // faccio un ciclo per leggere i blocchi individuati nella querystring
+    for(str in strList)
+    {
+      // divido ogni blocco mediante il simbolo uguale
+      // (uguale è usato per l'assegnazione del valore)
+      var parts = strList[str].split('=');
+      // inserisco nella array args l'accoppiata nome = valore di ciascun
+      // parametro presente nella querystring
+      args[unescape(parts[0])] = unescape(parts[1]);
+    }
+  }
+    
+    for(var v in args)
+        console.log(v);
+    
+  return args;
+}
