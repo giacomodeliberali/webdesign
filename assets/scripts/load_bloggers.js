bc5e3d66d13e143aaf0a0bd72890c6f410e255d4
Unknown <busatto.alessandro96@gmail.com>
2018-01-17 12:05:47 +0100

    Add Ciccio gamer (troll) date un occhiata

--- assets/scripts/load_bloggers.js
+++ assets/scripts/load_bloggers.js
@@ -1,18 +1,30 @@
 var blogger = new Array();
 blogger.push({
-    name: "Salvatore Aranzulla",
+   name: "Salvatore Aranzulla",
     rate: 7,
+    nart: 1323,
     specialità: "Hardware",
-    foto:"https://cdn3.iconfinder.com/data/icons/pixel-perfect-at-24px-volume-5/24/2049-512.png"
+    pres: "Salvatore Aranzulla è il fondatore del sito Aranzulla.it",
+    foto:"https://cdn3.iconfinder.com/data/icons/pixel-perfect-at-24px-volume-5/24/2049-512.png",
+    
 });
 
+blogger.push({
+   name: "Ciccio Gamer",
+    rate: 9999,
+    nart: 10,
+    specialità: "Videogame",
+    pres: "Ciccio Gamer è un boss",
+    foto:"https://cdn3.iconfinder.com/data/icons/pixel-perfect-at-24px-volume-5/24/2049-512.png",
+    
+});
 
 
 function generateMainBloggerHtml(objectBlogger) {
     return `
             <div class="row debug-layout">
                 <div class="col-sm-4 hidden-xs">
-                    <img src="${objectBlogger.foto} width="100%">
+                    <img src="${objectBlogger.foto}" width=100>
                 </div> 
                 <div class="col-sm-8" height="100%">
                     <div class="row">
@@ -25,7 +37,7 @@ function generateMainBloggerHtml(objectBlogger) {
                         </div>
                         <div class="row">
                             <div class="col-xs-8 col-sm-8">
-                                Numero articoli
+                                Numero articoli:${objectBlogger.nart}
                         </div>
                         <div class="col-xs-4 col-sm-4">
                             Rate: ${objectBlogger.rate}
@@ -38,7 +50,7 @@ function generateMainBloggerHtml(objectBlogger) {
                         </div>
                         <div class="row">
                             <div class="col-xs-12 col-sm-12">
-                            <b>Presentazione:</b>
+                            <b>Presentazione: ${objectBlogger.pres} </b>
                             </div>
                         </div>
                 </div>
