f8d6c8bfe30d4f5c0ae170bd471582f7c0e1af02
Unknown <busatto.alessandro96@gmail.com>
2018-01-17 16:28:12 +0100

    Migliorata pagina blogger, inserito css per blogger per centrare le img
    Da sistemare blogger css non riesco a centrare verticalmente l'immagine nel div

--- assets/scripts/load_bloggers.js
+++ assets/scripts/load_bloggers.js
@@ -19,12 +19,13 @@ blogger.push({
     
 });
 
+//width="100px" height="100px" per le immagini
 
 function generateMainBloggerHtml(objectBlogger) {
     return `
-            <div class="row debug-layout">
-                <div class="col-sm-4 hidden-xs">
-                    <img src="${objectBlogger.foto}" width=100>
+            <div class="row article debug-layout border-radius">
+                <div class="col-sm-4 hidden-xs ">
+                    <img src="${objectBlogger.foto}" width="100px" height="100px"> 
                 </div> 
                 <div class="col-sm-8" height="100%">
                     <div class="row">
@@ -32,27 +33,29 @@ function generateMainBloggerHtml(objectBlogger) {
                             <h3>${objectBlogger.name}</h3>
                         </div>
                         <div class="col-xs-4 col-sm-4">
-                                        #Follower
+                            <b>Rate: </b>${objectBlogger.rate}
                         </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8 col-sm-8">
+                            <b>Numero articoli: </b>${objectBlogger.nart}
                         </div>
-                        <div class="row">
-                            <div class="col-xs-8 col-sm-8">
-                                Numero articoli:${objectBlogger.nart}
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12">
+                            <b>Specialità: </b>${objectBlogger.specialità} 
                         </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12">
+                        <b>Presentazione:</b> ${objectBlogger.pres}
+                        </div>
+                    </div>
+                    <div class="row">
                         <div class="col-xs-4 col-sm-4">
-                            Rate: ${objectBlogger.rate}
+                            <b>#Follower:</b>
                         </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12">
-                                Specialità: ${objectBlogger.specialità} 
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12">
-                            <b>Presentazione: ${objectBlogger.pres} </b>
-                            </div>
-                        </div>
+                    </div>
                 </div>
     </div>
     `;
