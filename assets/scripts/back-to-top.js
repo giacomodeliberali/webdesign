8d7d9809025a62c41991afd3cfedb3816c0ca0a7
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2018-01-19 10:13:17 +0100

    Added back top top button to the document scroll

--- assets/scripts/back-to-top.js
+++ assets/scripts/back-to-top.js
@@ -2,12 +2,13 @@ $(document).ready(function () {
     scrollFunction();
     // When the user scrolls down 20px from the top of the document, show the button
     $("#articles-container").scroll(scrollFunction);
+    $(".container.main").scroll(scrollFunction);
 
     $("#backToTop").on("click", topFunction);
 });
 
 function scrollFunction() {
-    if ($("#articles-container").scrollTop() > 20) {
+    if ($("#articles-container").scrollTop() > 20 || $(".container.main").scrollTop() > 20) {
         document.getElementById("backToTop").style.display = "block";
     } else {
         document.getElementById("backToTop").style.display = "none";
@@ -17,4 +18,5 @@ function scrollFunction() {
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     $("#articles-container").animate({ scrollTop: 0 }, 200);
+    $(".container.main").animate({ scrollTop: 0 }, 200);
 }
