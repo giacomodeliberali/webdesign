11df0faaaec9e0bd1b93df023399fdc3217c68d6
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2018-01-20 15:58:11 +0100

    Added printable version of user.html

--- assets/scripts/load_preferred.js
+++ assets/scripts/load_preferred.js
@@ -1,24 +1,28 @@
 var titles = new Array();
 titles.push({
     title: "Buffer Overflow",
+    sub:"Le mille tecniche per il giusto exploit",
     scrLg: "http://www.apre.it/media/308462/security_assessment_logo.jpg",
     srcSm: "http://www.level3.com/_assets/build/img/icon/lockicon.png"
 });
 
 titles.push({
     title: "Pixel 2: Il migliore?",
+    sub:"Proviamolo con i benchmark di serie",
     scrLg: "https://www.technobuffalo.com/wp-content/uploads/2017/07/google-pixel-2-xl-concept-002.jpg",
     srcSm: "https://gd1.alicdn.com/imgextra/i4/2245691027/TB2nMqTevBNTKJjSszeXXcu2VXa_!!2245691027.png_50x50.jpg"
 });
 
 titles.push({
     title: "iPhone X: vulnerabilità sullo sblocco",
+    sub:"La tua identità non sarà mai più al sicuro",
     scrLg: "https://blog.yeppon.it/wp-content/uploads/2017/11/iPhone-X-Display.png",
     srcSm: "https://www.sv-comp.com/image/cache/catalog/data/apple_logo-50x50.jpg"
 });
 
 titles.push({
     title: "Ubuntu phone: innovazione.",
+    sub:"Fondere computer e telefono per un'esperienza unica di lavoro",
     scrLg: "https://www.lffl.org/wp-content/uploads/2017/09/ubuntu-4.jpg",
     srcSm: "https://www.virtualmaster.com/assets/logo/logo-ubuntu_cof-orange-hex_50-50-332f5ad5ac6e9988a152dbc6d42d6fc3.png"
 });
@@ -26,49 +30,56 @@ titles.push({
 
 titles.push({
     title: "iMac Pro: stellare",
+    sub:"Chi non ne desidererebbe uno, scopriamo tutte le features",
     scrLg: "http://cdn.mos.cms.futurecdn.net/R4PAweEta5PyquiLWQjCNA.jpg",
     srcSm: "https://www.sv-comp.com/image/cache/catalog/data/apple_logo-50x50.jpg"
 });
 
 titles.push({
     title: "Android 7: tutte le features",
+    sub:"Da ora l'invenzione si unisce ai dispositivi mobili",
     scrLg: "http://ksassets.timeincuk.net/wp/uploads/sites/54/2016/09/android-7-0-nougat-13-2.jpg",
     srcSm: "http://kissfm.com.au/wp-content/uploads/2013/08/Android-icon.png"
 });
 
 titles.push({
     title: "Go: il linguaggio Google più smart",
+    sub:"Smart nella scrittura, nella tecnica e nell'implementazione",
     scrLg: "https://goozernation.com/wp-content/uploads/2017/06/maxresdefault.jpg",
     srcSm: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Googlelogoi.png/50px-Googlelogoi.png"
 });
 
 titles.push({
     title: "Nuovi benchmark sui browser",
+    sub:"Ogni anno la stessa storia, o forse no?",
     scrLg: "http://www.namerific.com/blog/wp-content/uploads/2016/09/maxresdefault.jpg",
     srcSm: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Firefox_Logo%2C_2017.svg/50px-Firefox_Logo%2C_2017.svg.png"
 });
 
 titles.push({
-    title: "Watchdogs",
+    title: "Watchdogs 2",
+    sub:"San Francisco non ha più porte chiuse per Marcus",
     scrLg: "https://gaming.hwupgrade.it/articoli/videogames/4815/wd1.jpg",
     srcSm: "https://ae01.alicdn.com/kf/HTB1iOhUPVXXXXauapXXq6xXFXXXW/00-Watch-Dogs-2-Dedsec-Aiden-Pearce-Wrench-Mask-Helmet-Eyepatch-Face-Muffle-Cosplay.jpg_50x50.jpg"
 });
 
 titles.push({
-    title: "call of duty WW2 e il suo motore grafico",
+    title: "Call of duty WW2 e il suo motore grafico",
+    sub:"Giudicato troppo intenso anche dai giogatori usuali, ma perchè?",
     scrLg: "https://www.playstationzone.it/wp-content/uploads/2017/10/aq3r313w8zsy.png",
     srcSm: "https://usercontent1.hubstatic.com/12809778_50.jpg"
 });
 
 titles.push({
     title: "Nvidia gtx 1080 è VR ready ",
+    sub:"Per immergersi nel mondo virtuale è necessaria una scheda spaziale",
     scrLg: "https://images.everyeye.it/img-singole/articolo-49846.jpg",
     srcSm: "https://static-cdn.jtvnw.net/jtv_user_pictures/nvidiageforcede-profile_image-64f12030b4ecc178-50x50.png"
 });
 
 function generatePreferredHtml(objectTitle, id) {
     return `
-        <div class="row article debug-layout border-radius">
+        <div class="row article debug-layout border-radius hidden-print">
                 <article id="art${id}">
                     <div class="row">
                         <div class="col-sm-6 col-md-4">
@@ -103,6 +114,20 @@ function generatePreferredHtml(objectTitle, id) {
                     </div>
                     <button type="button" class="btn btn-danger btn-sm remove-article" onclick="removeArticle('art${id}')"><i class="fa fa-times"></i></button>
                 </article>
+        </div>
+
+        <div class="row visible-print-block">
+            <div class="col-xs-4">
+                <img src="${objectTitle.srcSm}" width="100%">
+            </div>
+            <div class="col-xs-8">
+                <div class="row">
+                    <h1>${objectTitle.title}</h1>
+                </div>
+                <div class="row">
+                    <h3>${objectTitle.sub}</h3>
+                </div>
+            </div>
         </div>
     `;
 }
